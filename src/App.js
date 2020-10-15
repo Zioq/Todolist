e363854d1b27e0f06c71ad59064599744bc5efd7
Zioq <jr.roberthan@gmail.com>
2020-10-15 12:49:53 -0700

    update input text and redering

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+import "./App.css";
+
+//Import components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+
+  const [inputText,setInputText] = useState("");
+  const [todos,setTodos] = useState([]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Robert Todo List</h1>
+      </header>
+      <Form inputText = {inputText} todos= {todos} setTodos = {setTodos} setInputText = {setInputText}/>
+      <TodoList todos = {todos}/>
+    </div>
+  );
+}
+
+export default App;
