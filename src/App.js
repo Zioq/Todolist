179712694e61314121acb86b9706cc0fda745480
Zioq <jr.roberthan@gmail.com>
2020-10-17 16:29:20 -0700

    update localstorage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
-
-
   /*[STATES] */
   //Get the piece of input data what we need
   //inputText: actual Data
@@ -21,16 +19,21 @@ function App() {
   // Set the filter state
   const [filteredTodos, SetFilterTodos] = useState([]); // What the this things gonna do is that when the todos are completed, their 3th State(about Status (All,Complete,Uncompleted) ) will be move into this 4rd State that we're going to render out in the end.
 
+  //RUN ONCE when the app start
+  useEffect(() => {
+    getLocalTodos();
+  }, []); // use [] as second parameter because we wnat to only runs once.
+
   // USE EFFECT
-  // first argument is consolelog funciton and second argugment array. 
+  // first argument is consolelog funciton and second argugment array.
   // this array function is going to run only once when the compoent is first rendered
   // Here is the power of useEffect: you can re-run this function if you want by adding a value in the second array arguement. For example every time we hit submit you want to run the this function again. In this case I want to evety time I add a new todo this useEffect runs. this function will be run every time todo's value changes
   // We can add mutiple value in second parameter
-  useEffect(()=> {
+  useEffect(() => {
     //console.log('Use Effect run');
     filterHandler();
-  }, [todos,status]); 
-
+    saveLocalTodos();
+  }, [todos, status]);
 
   // Functions
   const filterHandler = () => {
@@ -50,6 +53,20 @@ function App() {
     }
   };
 
+  // Save to Local
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'));
+      console.log(todoLocal);
+      setTodos(todoLocal);
+    }
+  };
 
   return (
     <div className="App">
@@ -66,7 +83,11 @@ function App() {
         setStatus={setStatus}
       />
       {/** Passing down the todos data */}
-      <TodoList setTodos={setTodos} todos={todos} filteredTodos= {filteredTodos}/>
+      <TodoList
+        setTodos={setTodos}
+        todos={todos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
